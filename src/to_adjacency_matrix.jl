a31f5f785f798c46ada906fc3927ab8408ca4cb2
Charun Upara <uparacha@grinnell.edu>
2020-06-15 08:07:34 -0500

    clean up and document to_adjacency_matrix

---
+++ src/to_adjacency_matrix.jl
@@ -0,0 +1,43 @@
+include("Hypergraphs.jl")
+include("RandomHypergraphs.jl")
+using MatrixNetworks
+
+# TODO: Convert to MatrixNetworks type
+
+"""
+`to_adjacency_matrix`
+=====================
+
+Convert a VertexHyperA to an adjacency matrix
+
+Arguments
+----------
+    - `A::VertexHyperA : The VertexHyperA that will be converted into an adjacency matrix`
+
+Examples
+----------
+~~~~
+to_adjacency_matrix(VertexHyperA([[1,2,3,4], [3,4,1], [1,4,5]], ["One", "Two", "Three", "Four", "Five"], 5, 3))
+~~~~
+"""
+function to_adjacency_matrix(A::VertexHypergraph)
+    adjacency_matrix = zeros(Int64, A.n, A.n) # create adjacency matrix of size n x n
+
+    # loop through each hyperedge and create edges in the new adjacency matrix
+    for i in 1:A.m
+        for j in 1:size(A.edges[i],1)
+            for k in 1:size(A.edges[i],1)
+                if j == k
+                    continue # pass because we don't want self-loops
+                else
+                    index1 = A.edges[i][j]
+                    index2 = A.edges[i][k]  
+                    adjacency_matrix[index1,index2] = 1
+                end
+            end
+        end
+    end
+    return adjacency_matrix
+end
+
+
