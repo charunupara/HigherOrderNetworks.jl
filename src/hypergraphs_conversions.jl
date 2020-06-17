ae53f9a8fed4e6150994f84915c7b5ca69baa84f
Joshua Turner <turnerjo@grinnell.edu>
2020-06-17 14:38:18 -0500

    Option to specify simple graph vs. loopy multigraph
    Simple graphs have at most one edge between two nodes and no self-loops. Loopy multigraphs, on the other hand, allow both of these.
    In terms of the matrix, this means that loopy multigraphs can have non-zero entries on the diagonal and values greater than 1 in any entry.

--- src/hypergraphs_conversions.jl
+++ src/hypergraphs_conversions.jl
@@ -12,31 +12,40 @@ Convert a VertexHypergraph to an adjacency matrix
 
 Arguments
 ----------
-    - `A::VertexHypergraph : The VertexHypergraph that will be converted into an adjacency matrix`
+    - `h::Hypergraphs`: The hypergraph that will be converted into an adjacency matrix
+    - `simple::Bool (=false)`: Whether to exclude multi-edges and self-loops from the matrix (they are included by default)
 
 Examples
 ----------
 ~~~~
-to_adjacency_matrix(VertexHypergraph([[1,2,3,4], [3,4,1], [1,4,5]], ["One", "Two", "Three", "Four", "Five"], 5, 3))
+to_adjacency_matrix(VertexHypergraph([[1,2,3,4], [3,4,1], [1,4,5]], ["One", "Two", "Three", "Four", "Five"], 5, 3); simple=true)
 ~~~~
 """
-function to_adjacency_matrix(A::VertexHypergraph)
-    adjacency_matrix = zeros(Int64, A.n, A.n) # create adjacency matrix of size n x n
+function to_adjacency_matrix(h::Hypergraphs; simple::Bool=false)
+    adjacency_matrix = zeros(Int64, h.n, h.n) # Create adjacency matrix of size n x n
 
-    # loop through each hyperedge and create edges in the new adjacency matrix
-    for i in 1:A.m
-        for j in 1:size(A.edges[i],1)
-            for k in 1:size(A.edges[i],1)
-                if j == k
-                    continue # pass because we don't want self-loops
+    # Loop through each hyperedge and create edges in the new adjacency matrix
+    for i = 1:h.m
+        for j = 1:size(h.edges[i],1)-1
+            for k = j+1:size(h.edges[i],1)
+                index1 = vertex_floor(h.edges[i][j])
+                index2 = vertex_floor(h.edges[i][k])
+                if simple
+                    adjacency_matrix[index1,index2] = adjacency_matrix[index2,index1] = 1 # Setting to 1 excludes multi-edges
                 else
-                    index1 = A.edges[i][j]
-                    index2 = A.edges[i][k]  
-                    adjacency_matrix[index1,index2] = 1
+                    adjacency_matrix[index1,index2] += 1 # Incrementing tracks edge multiplicity
+                    adjacency_matrix[index2,index1] += 1
                 end
             end
         end
     end
+
+    if simple # Remove self-loops
+        for i = 1:h.n
+            adjacency_matrix[i][i] = 0
+        end
+    end
+
     return adjacency_matrix
 end
 
@@ -50,14 +59,15 @@ Convert a VertexHypergraph to a MatrixNetwork
 
 Arguments
 ----------
-    - `A::VertexHypergraph : The VertexHypergraph that will be converted into a MatrixNetwork`
+    - `h::Hypergraphs`: The hypergraph that will be converted into a MatrixNetwork
+    - `simple::Bool (=false)`: Whether to exclude multi-edges and self-loops from the MatrixNetwork (they are included by default)
 
 Examples
 ----------
 ~~~~
-hypergraph_to_matrixnetwork(VertexHypergraph([[1,2,3,4], [3,4,1], [1,4,5]], ["One", "Two", "Three", "Four", "Five"], 5, 3))
+hypergraph_to_matrixnetwork(VertexHypergraph([[1,2,3,4], [3,4,1], [1,4,5]], ["One", "Two", "Three", "Four", "Five"], 5, 3); simple=true)
 ~~~~
 """
-function hypergraph_to_matrixnetwork(A::VertexHypergraph)
-    return MatrixNetwork(sparse(to_adjacency_matrix(A)))
+function hypergraph_to_matrixnetwork(h::Hypergraphs; simple=false)
+    return MatrixNetwork(sparse(to_adjacency_matrix(h; simple=simple)))
 end
