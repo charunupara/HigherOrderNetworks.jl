a5d799c1d74f7c24969fcd367ac703632fb73469
Joshua Turner <turnerjo@grinnell.edu>
2020-07-24 16:59:37 -0500

    Community detection for general motifs

--- src/motif_communities.jl
+++ src/motif_communities.jl
@@ -1,5 +1,16 @@
 using MatrixNetworks
 include("Utilities.jl")
+
+"""
+Common motif generators
+    - `path(k::Int64)`: Returns a path of length `k`
+    - `cycle(k::Int64)`: Returns a cycle of length `k`
+    - `clique(k::Int64)`: Returns a clique of size `k`
+"""
+path(k::Int64) = MatrixNetwork([i for i = 1:k], [j+1 for j = 1:k])
+cycle(k::Int64) = MatrixNetwork([i for i = 1:k], [[j+1 for j = 1:k-1]; [1]])
+clique(k::Int64) = MatrixNetwork([i for i = 1:k for j = 1:k-1], [j for i = 1:k for j = 1:k if j != i])
+
 """
 `motif_modularity`
 ==================
@@ -47,7 +58,7 @@ function motif_modularity(A::MatrixNetwork, C::Vector{Vector{Int64}}; M::MatrixN
                 return
             end
         end
-        if any([inds[x] == inds[y] for x = 1:length(inds) for y = x+1:length(inds)])
+        if any([inds[x] == inds[y] for x = 1:length(inds) for y = x+1:length(inds)]) # Requires unique indices
             inc_inds!(inds)
         end
     end
@@ -99,8 +110,7 @@ function motif_modularity(A::MatrixNetwork, C::Vector{Vector{Int64}}; M::MatrixN
 
         inc_inds!(indices)
     end
-    println(shared_strengths)
-    #println("$C: $w/$W,$u/$U")
+
     return (w/W) - (u/U), shared_strengths, W, U
 end
 
@@ -122,45 +132,29 @@ function louvain_motif(A::MatrixNetwork; M::MatrixNetwork=MatrixNetwork([1],[2])
 
     mod, strengths, W, U = motif_modularity(A,C; M=M) # Initial values for modularity shortcut
 
-    ### Merge the communities at indices i and j in C. Updates both C and strengths. ###
-    function merge_communities!(i,j) # TODO: For merges need to take into account 'imaginary' motifs for n_ij
-        if i == j return end # Can't merge a community with itself!
-
+    ### Collapse all communities in the group into one. Updates C and strengths ###
+    function merge_community_group!(group)
         new_groups = collect(deepcopy(keys(strengths)))
         cpy = deepcopy(new_groups) # Remember old keys so we can get rid of ones that no longer exist at the end
 
         new_strengths = Dict()
-        covered = Set() # Pairwise relationships that have already been used in updates
+
+        i = group[1]
 
         for k = 1:size(new_groups,1)
             g = new_groups[k]
-            g[g.==j] .= i # Replace j with i
+            for c in group
+                g[g.==c] .= i # Replace all other group members with i
+            end
 
-            g = sort(collect(Set(g))) # If i already included, remove duplicate and sort for consistency
+            g = sort(collect(Set(g))) # If i already included, remove duplicates and sort for consistency
 
-            if length(reduce(vcat, C[g])) < M.n  || g in keys(new_strengths) continue end # Don't do anything for degenerate groups or repeats
-            #if length(g) < 2 || g in keys(new_strengths) continue end # Don't do anything for degenerate groups or repeats
+            if length(g) < 2 continue end # Leave out degenerate groups
 
-            if i in g # Since g contains the new merged community, we have to update its dict values
-                new_strengths[g] = [0,0]
-
-                for c in g
-                    if c == i continue end # No change needed for [i,i]
-                    ic = sort([i,c])
-                    jc = sort([j,c])
-
-                    if !(ic in covered) && haskey(strengths, ic) # ic is new and i and c have a relationship TODO AMBIGUOUS!!
-                        new_strengths[g] .+= strengths[ic] # Update values
-                        push!(covered, ic)
-                    end
-
-                    if !(jc in covered) && haskey(strengths, jc)
-                        new_strengths[g] .+= strengths[jc]
-                        push!(covered, jc)
-                    end
-                end
+            if !haskey(new_strengths, g)
+                new_strengths[g] = strengths[cpy[k]]
             else
-                new_strengths[g] = strengths[cpy[k]] # g doesn't contain the new community, so we leave its values alone
+                new_strengths[g] += strengths[cpy[k]]
             end
         end
 
@@ -171,62 +165,45 @@ function louvain_motif(A::MatrixNetwork; M::MatrixNetwork=MatrixNetwork([1],[2])
         old_keys = collect(deepcopy(keys(new_strengths)))
         new_keys = []
         vals = []
-        for k = 1:length(old_keys) # Relabel community relationships to account for the loss of j
+        for k = 1:length(old_keys) # Relabel community relationships to account for merge
             key = old_keys[k]
             push!(vals, new_strengths[key])
             for r = 1:length(key)
-                if key[r] > j
-                    key[r] -= 1
+                diff = 0
+                for c in group[2:length(group)]
+                    if key[r] > c
+                        diff += 1
+                    end
                 end
+                key[r] -= diff
             end
+
             push!(new_keys, key)
         end
 
         # Update partition
-        C[i] = [C[i]; C[j]]
-        setdiff!(C, [C[j]])
+        C[i] = sort(reduce(vcat, C[group]))
+        for c in group[2:length(group)] setdiff!(C, [C_last[c]]) end
 
         strengths = Dict(new_keys[i] => vals[i] for i = 1:length(new_keys)) # Ta da!
     end
 
-    ### Perform one iteration of the modified Louvain algorithm. For each community, finds the merge that most increases modularity, then performs it. ###
+    ### Perform one iteration of the modified Louvain algorithm. Finds the group of communities that, if collapsed into one, would most increase modularity and performs it. ###
     function louvain_iter!()
-        for i = 1:size(C,1)
-            if i > size(C,1) break end
-            pre_mod = mod # Initial modularity, used to compute the the modularity resulting from a hypothetical merge
-            best_dQ = typemin(Float64)
-            best = i # Keep track of the community with which merging would maximally increase modularity
-            for j = 1:size(C,1)
-                if i == j continue end # Merging with self doesn't change modularity, so we can skip
-                if j > size(C,1) break end
-
-                new_C = deepcopy(C) # Copy of C used to simulate merging i and j
-                ordered = sort([i,j])
-
-                # Perform the merge
-                new_C[i] = [C[i]; C[j]]
-                setdiff!(new_C, [C[j]])
-
-                if !(ordered in keys(strengths)) # i and j have no relationship, so merging them won't change modularity # TODO: This is where I assume edges
-                    dQ = 0.0
-                else
-                    dQ = (strengths[ordered][1]/W) - (strengths[ordered][2]/U) # Change in modularity from performing merge
-                end
-                #real_mod, s = motif_modularity(A, new_C; M=M)[1:2]
-
-                post_mod = pre_mod + dQ
-                println("$(C[i]), $(C[j]), $dQ")
-                if post_mod > mod
-                    mod = post_mod
-                    best = j
-                    #println(strengths)
-                end
+        max_dQ = 0.0 # The merge must increase modularity
+        best = []
+        for k in filter(x -> strengths[x][1] > 0, keys(strengths))
+            dQ = (strengths[k][1]/W) - (strengths[k][2]/U)
+            if dQ > max_dQ
+                max_dQ = dQ
+                best = copy(k)
             end
-            merge_communities!(i, best) # Perform best merge
         end
+        if !isempty(best) merge_community_group!(best) end
+        mod += max_dQ
     end
-
     louvain_iter!() # Get the ball rolling, make C different from C_last
+
     while C_last != C # Stop merging when no merge increases modularity
         C_last = deepcopy(C)
         louvain_iter!()
@@ -235,16 +212,19 @@ function louvain_motif(A::MatrixNetwork; M::MatrixNetwork=MatrixNetwork([1],[2])
     return map(sort, C)
 end
 
-A = MatrixNetwork([(1,2), (1,3), (2,1), (2,3), (2,4), (3,1), (3,2), (4,2), (4,5), (4,8), (5,4), (5,6), (5,7), (6,5), (6,7), (7,5), (7,6), (8,4), (8,9), (8,10), (9,8), (9,10), (10,9), (10,8)], 10)
+#A = MatrixNetwork([(1,2), (1,3), (2,1), (2,3), (2,4), (3,1), (3,2), (4,2), (4,5), (4,8), (5,4), (5,6), (5,7), (6,5), (6,7), (7,5), (7,6), (8,4), (8,9), (8,10), (9,8), (9,10), (10,9), (10,8)], 10)
 #A = MatrixNetwork([1,1,2,2,3,3,3,4],[2,3,1,3,1,2,4,3])
 println("START")
 println("")
+#println(louvain_motif(A)) # Magically, this references the first A, even though it's commented out???
 
-@time println(louvain_motif(A))
+star = MatrixNetwork([1,1,1,1,1,1,1,1,2,3,4,5,6,7,8,9], [2,3,4,5,6,7,8,9,1,1,1,1,1,1,1,1])
+#println(louvain_motif(star; M=path(2))) # Incorrect, somehow he leaves out the middle node in the paper. I think it has to do with TODO #1 down below
 
 tri_circ = MatrixNetwork([1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,7,7,8,8,9,9,10,10],
                          [2,3,4,5,1,3,4,5,1,2,4,5,1,2,3,5,1,2,3,4,6,5,7,10,6,8,7,9,8,10,9,6])
-#println(louvain_motif(tri_circ; M=MatrixNetwork([1,2,3], [2,3,1])))
+#println(louvain_motif(tri_circ; M=path(3)))
+
 karate_edges = [[2,1],[3,1],[3,2],[4,1],[4,2],[4,3],[5,1],[6,1],[7,1],[7,5],[7,6],[8,1],[8,2],[8,3],[8,4],[9,1],
 [9,3],[10,3],[11,1],[11,5],[1,6],[12,1],[13,1],[13,4],[14,1],[14,2],[14,3],[14,4],[17,6],[17,7],[18,1],[18,2],
 [20,1],[20,2],[22,1],[22,2],[26,24],[26,25],[28,3],[28,24],[28,25],[29,3],[30,24],[30,27],[31,2],[31,9],[32,1],
@@ -252,11 +232,7 @@ karate_edges = [[2,1],[3,1],[3,2],[4,1],[4,2],[4,3],[5,1],[6,1],[7,1],[7,5],[7,6
 [34,10],[34,14],[34,15],[34,16],[34,19],[34,20],[34,21],[34,23],[34,24],[34,27],[34,28],[34,29],[34,30],[34,31],[34,32],[34,33]]
 karate_edges = reduce(vcat, [[i, reverse(i)] for i in karate_edges])
 karate_network = MatrixNetwork(map(x->x[1],karate_edges),map(x->x[2],karate_edges))
-#@time println(louvain_motif(karate_network))
+@time println(louvain_motif(karate_network; M=path(2)))
 
-# TODO: Community detection works for basic case where M is a single edge, but fails with larger motifs, usually having made no merges. Where do I implicitly assume edges in my implementation?
-# TODO: Our algorithm is slightly different from the Louvain in that Louvain checks FOR EVERY COMMUNITY, if it would be best to leave its current community. Might implement this
-# TODO: Don't count degenerated motifs!
-
-# Doing a dyadic merge, at least initially, will only decrease modularity, as we increase intra-community nullcase without increase intra-community motifs.
-# Merge hyperedges?
+# TODO: Include option of specifying which edges k_delt should apply to
+# TODO: Efficient motif detection: find all nodes that have degree >= highest degree of motif and count motifs incident. Similar to Chiba-Nishezki
